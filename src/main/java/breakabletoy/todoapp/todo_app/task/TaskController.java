ad7da136208c9a64545cbd21bd289c02a2f6dd4c
Jose Espericueta <jose.espericueta@ENCEDCSLPLT0791.local>
2025-02-07 21:10:12 -0600

    Added CRUD functionality for tasks

--- src/main/java/breakabletoy/todoapp/todo_app/task/TaskController.java
+++ src/main/java/breakabletoy/todoapp/todo_app/task/TaskController.java
@@ -1,14 +1,12 @@
     package breakabletoy.todoapp.todo_app.task;
 
     import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.web.bind.annotation.GetMapping;
-    import org.springframework.web.bind.annotation.RequestMapping;
-    import org.springframework.web.bind.annotation.RestController;
+    import org.springframework.web.bind.annotation.*;
 
     import java.util.List;
 
     @RestController
-    @RequestMapping
+    @RequestMapping("/tasks")
     public class TaskController {
 
         private final TaskService taskService;
@@ -21,5 +19,20 @@
         @GetMapping
         public List<Task> getTasks(){
             return taskService.getTasks();
+        }
+
+        @PostMapping
+        public void addTask(@RequestBody Task task){
+            taskService.addTask(task);
+        }
+
+        @PutMapping("/{id}")
+        public void editTask(@PathVariable Long id, @RequestBody Task updatedTask){
+            taskService.editTask(id, updatedTask);
+        }
+
+        @DeleteMapping("{id}")
+        public void deleteTask(@PathVariable Long id){
+            taskService.deleteTask(id);
         }
     }
--- src/main/java/breakabletoy/todoapp/todo_app/task/TaskService.java
+++ src/main/java/breakabletoy/todoapp/todo_app/task/TaskService.java
@@ -1,28 +1,53 @@
 package breakabletoy.todoapp.todo_app.task;
 
-import breakabletoy.todoapp.todo_app.Priority;
+import lombok.Getter;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
-import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class TaskService {
 
-    @GetMapping
-    public List<Task> getTasks(){
-        return List.of(
-                new Task(
-                        1L,
-                        "First task created",
-                        LocalDate.of(2025, Month.FEBRUARY, 20),
-                        Boolean.TRUE,
-                        LocalDate.of(2025, Month.FEBRUARY, 15),
-                        Priority.High,
-                        LocalDate.of(2025, Month.FEBRUARY, 6)
-                )
-        );
+    @Getter
+    private final List<Task> tasks = new ArrayList<>();
+    private final AtomicLong idGenerator = new AtomicLong(1);
+
+    public void addTask(Task task){
+    task.setId(idGenerator.getAndIncrement());
+    tasks.add(task);
+    }
+
+    public void editTask(Long id, Task updatedTask){
+        Task task = tasks.stream().filter(existingTask -> existingTask.getId().equals(id)).findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(("Task not found")));
+
+        if(updatedTask.getText() == null || updatedTask.getText().trim().isEmpty()){
+            throw new IllegalArgumentException("Text cannot be empty");
+        }
+
+        if(updatedTask.getDueDate().isBefore(updatedTask.getCreationDate())){
+            throw new IllegalArgumentException("Due date cannot be earlier than the creation date");
+        }
+
+        task.setText(updatedTask.getText());
+        task.setDueDate(updatedTask.getDueDate());
+        task.setDoneFlag(updatedTask.getDoneFlag());
+        task.setPriority(updatedTask.getPriority());
+
+        if(updatedTask.getDoneFlag() && task.getDoneDate() == null){
+            task.setDoneDate(LocalDate.now());
+        }else if (!updatedTask.getDoneFlag()){
+            task.setDoneDate(null);
+        }
+    }
+
+    public void deleteTask(Long id){
+        boolean removed = tasks.removeIf(task -> task.getId().equals(id));
+        if(!removed){
+            throw new IllegalArgumentException("Task with id: " + id + "not found");
+        }
     }
 }
